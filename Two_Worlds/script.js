109a0ddd41267fc21aa7e7c53f3843f609d5c544
Sara-E-Walker <50123761+Sara-E-Walker@users.noreply.github.com>
2022-08-04 23:17:47 -0700

    Two Worlds update

--- Two_Worlds/script.js
+++ Two_Worlds/script.js
@@ -5,21 +5,9 @@ const highlight = document.createElement('span');
 document.body.appendChild(highlight);
 highlight.classList.add('highlight');
 
-// Set initial dimensions and position of 'highlight' based on activeButton coords 
-function initialHightlightLocation() {
+// Checks if button is active 
+function isButtonActive() {
   const activeButton = document.querySelector('.button--is-active');
-  const activeButtonCoords = activeButton.getBoundingClientRect();
-
-  const initialCoords = {
-    width: activeButtonCoords.width,
-    height: activeButtonCoords.height,
-    left: activeButtonCoords.left + window.scrollX,
-    top: activeButtonCoords.top + window.scrollY
-  }
-
-  highlight.style.width = `${initialCoords.width}px`;
-  highlight.style.height = `${initialCoords.height}px`;
-  highlight.style.transform = `translate(${initialCoords.left}px, ${initialCoords.top}px)`;
 }
 
 function handleClick(e) {
@@ -27,18 +15,6 @@ function handleClick(e) {
 
   buttons.forEach(button => button.classList.remove('button--is-active'));
   this.classList.add('button--is-active');
-
-  // Set current dimensions and position of 'highlight' based on the clicked button 
-  const buttonCoords = this.getBoundingClientRect();
-  const coords = {
-    width: buttonCoords.width,
-    height: buttonCoords.height,
-    left: buttonCoords.left + window.scrollX,
-    top: buttonCoords.top + window.scrollY
-  }
-  highlight.style.width = `${coords.width}px`;
-  highlight.style.height = `${coords.height}px`;
-  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 
   // Show the menu associated to the clicked button
   const targetMenu = document.querySelector(`#${this.dataset.target}`);
@@ -48,6 +24,5 @@ function handleClick(e) {
   })
 }
 
-window.addEventListener('load', initialHightlightLocation);
-window.addEventListener('resize', initialHightlightLocation);
+window.addEventListener('load', isButtonActive);
 buttons.forEach(button => button.addEventListener('click', handleClick));
