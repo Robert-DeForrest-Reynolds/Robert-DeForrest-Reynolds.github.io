7d7d9b5b741ddab117824882e3b64f3d1bd6fbc5
Sara-E-Walker <50123761+Sara-E-Walker@users.noreply.github.com>
2022-08-05 17:30:42 -0700

    Two Worlds update

--- Two_Worlds/script.js
+++ Two_Worlds/script.js
@@ -1,10 +1,6 @@
 const buttons = document.querySelectorAll('.button');
 const menus = document.querySelectorAll('.menu');
 
-const highlight = document.createElement('span');
-document.body.appendChild(highlight);
-highlight.classList.add('highlight');
-
 // Checks if button is active 
 function isButtonActive() {
   const activeButton = document.querySelector('.button--is-active');
