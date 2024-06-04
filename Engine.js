7c1452d49bb5351bc4e3f1f2c57588b28ac4ce9f
robert-deforrest-reynolds <rldr.europa@gmail.com>
2024-06-04 07:55:42 -0700

    Some stuff from months ago

---
+++ Engine.js
@@ -0,0 +1,6 @@
+const VariableExample = document.getElementById("VariableExample");
+var VariableExampleString = "\
+String Apple_As_String = \"Apple\"\n\
+Integer Five = 5\n";
+
+VariableExample.innerHTML = VariableExampleString;
