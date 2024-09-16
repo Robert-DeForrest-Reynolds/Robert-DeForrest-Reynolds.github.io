6d3463c9f3018bc19012062b043d66137bff5d77
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2024-09-15 23:12:53 -0700

    stuff

--- WeddingRSVP.js
+++ WeddingRSVP.js
@@ -1,5 +1,14 @@
 const YesCheck = document.getElementById("YesCheck");
 const NoCheck = document.getElementById("NoCheck");
+const YesCheckGuests = document.getElementById("YesCheckGuests")
+
+
+var GuestInputsLoaded = false
+var GuestNameMax = 8
+
+// for (let Iteration = 0; Iteration < GuestNameMax; Iteration++){
+//   document.getElementById("GuestName" + Iteration).style.display = none
+// }
 
 YesCheck.addEventListener("change", function() {
   if (YesCheck.checked) {
@@ -16,3 +25,24 @@ NoCheck.addEventListener("change", function() {
     }
   }
 });
+
+YesCheckGuests.addEventListener("change", function() {
+  if (YesCheckGuests) {
+    if (GuestInputsLoaded == false){
+      GuestInputsLoaded = true
+      Load_Guest_Inputs()
+    }
+  }
+})
+
+function Load_Guest_Inputs() {
+  console.log("Loading guest")
+  for (let Iteration = 1; Iteration <= GuestNameMax; Iteration++)
+  {
+    GuestElement = document.getElementById("GuestName"+Iteration)
+    GuestElement.style.display = "inline";
+    if (Iteration % 2 == 0){
+      GuestElement.insertAdjacentElement("afterend", document.createElement('br'))
+    }
+  }
+}
